59c1896bd9e09f3f0cd1f3bff6993dfe7b14c254
test <test@example.com>
2021-04-17 05:04:47 +0000

    Add about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,9 @@ class StaticPagesController < ApplicationController
   def help
     # => app/views/static_pages/help.html.erb
   end
+
+  def about
+    # => app/views/static_pages/about.html.erb
+  end
+
 end
