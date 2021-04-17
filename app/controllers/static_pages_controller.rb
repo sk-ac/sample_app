66ac70dfe744492268b73446740134ceab6c143a
test <test@example.com>
2021-04-17 04:39:52 +0000

    Add a Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    # => app/views/static_pages/home.html.erb
+  end
+
+  def help
+    # => app/views/static_pages/help.html.erb
+  end
+end
