8228eb98ab1f90c6410ec622f2d95def3402dc87
test <test@example.com>
2021-04-17 06:14:40 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,8 @@ class StaticPagesController < ApplicationController
     # => app/views/static_pages/about.html.erb
   end
 
+  def contact
+    # => app/views/static_pages/contact.html.erb
+  end
+
 end
